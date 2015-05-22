c53ed5f3f5f7885b06a13467cd53c0890776ff04
magerate <magerate1129@gmail.com>
2015-05-22 09:17:35 +0800

    add WallSurfaceDrawer

---
+++ Cession.MonoTouch/Drawing/WallSurfaceDrawer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CoreGraphics;
+using UIKit;
+
+using Cession.Diagrams;
+using Cession.Geometries;
+using D = Cession.Diagrams;
+
+namespace Cession.Drawing
+{
+    public class WallSurfaceDrawer:ShapeDrawer
+    {
+        protected override void DoDraw (DrawingContext drawingContext, Shape shape)
+        {
+            var wallSurface = shape as WallSurface;
+            if (null == wallSurface)
+                throw new ArgumentException ("shape");
+
+            drawingContext.PushTransform (wallSurface.Transform);
+            drawingContext.StrokeRect (wallSurface.Bounds);
+            drawingContext.PopTransform ();
+        }
+    }
+}
+
