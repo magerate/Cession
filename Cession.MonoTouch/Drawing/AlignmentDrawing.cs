58284f39373657aaf8b78f8b75c47077a8ce58e3
magerate <magerate1129@gmail.com>
2015-05-28 16:21:12 +0800

    n

--- Cession.MonoTouch/Drawing/AlignmentDrawing.cs
+++ Cession.MonoTouch/Drawing/AlignmentDrawing.cs
@@ -10,20 +10,7 @@ namespace Cession.Drawing
 {
     public static class AlignmentDrawing
     {
-        public static void Draw (this DevicePointToPointRule rule,
-                          CGContext context,
-                          Matrix transform)
-        {
-            if (!rule.IsAligned)
-                return;
 
-            var point = transform.Transform (rule.ReferencePoint.Value).ToCGPoint ();
-            context.SaveState ();
-            context.SetAlpha (.4f);
-            UIColor.Blue.SetFill ();
-            context.FillCircle (point, 16);
-            context.RestoreState ();
-        }
     }
 }
 
