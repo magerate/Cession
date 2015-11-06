caee32df153dc7fc11ae7fbcc3550ecef77e6693
magerate <magerate1129@gmail.com>
2015-11-06 11:28:39 +0800

    n

---
+++ Geometries/PolygonAlgorithm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cession.Geometries
+{
+    public static class PolygonAlgorithm
+    {
+        public static bool Contains (IList<Point> polygon, Point point)
+        {
+            if (null == polygon)
+                throw ArgumentNullException ("polygon");
+            
+            if (polygon.Count < 3)
+                return false;
+            
+            int windNumber = GetWindNumber (polygon, point);
+            return windNumber != 0;
+        }
+
+        public static int GetWindNumber (IList<Point> polygon, Point point)
+        {
+            int windNumber = 0;
+
+            for (int i = 0; i < polygon.Count; i++)
+                if (polygon [i] <= point.Y)
+                {         
+                    if (polygon [i + 1].Y > point.Y)
+                    {
+                        if (Triangle.GetSignedArea (polygon [i], polygon [i + 1], point) < 0)
+                            windNumber++;
+                    }
+                }
+                else
+                {                        
+                    if (Triangle.GetSignedArea (polygon [i], polygon [i + 1], point) < 0)
+                        windNumber--;           
+                }
+            }
+            return windNumber;
+        }
+    }
+}
+
